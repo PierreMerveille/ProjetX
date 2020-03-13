63f9f464da31075e9df71054ddb77a9f9bb35603
JonathanReid <55463001+JonathanReid@users.noreply.github.com>
2020-03-13 09:48:35 +0100

    Lolz

--- Play.py
+++ Play.py
@@ -562,22 +562,16 @@ def move (move_list, ships, team, board, units_stats, peaks) :
                 del board[old_coord]['list_entity'][index]
 
             else:
-                print(new_coord[0])
-                print(new_coord[1])
-                print(old_coord[0])
-                print(old_coord[1])
-                                                                 
+
                 if ships[instruction[0]]['energy_point'] < max(abs(new_coord[0] - old_coord[0]), abs(new_coord[1] - old_coord[1])) * units_stats[team]['cruiser']['move'] :
                     print('Not enough energy_point in' + instruction[0])
                 else:
-                    change_value(instruction[0], ships, peaks, (ships[instruction[0]]['energy_point'] - abs(max(abs(new_coord[0] - old_coord[0]), abs(new_coord[1] - old_coord[1])) * units_stats[team]['cruiser']['move'])), 'energy_point', units_stats,team)
+                    change_value(instruction[0], ships, peaks, (ships[instruction[0]]['energy_point'] - (max(abs(new_coord[0] - old_coord[0]), abs(new_coord[1] - old_coord[1])) * units_stats[team]['cruiser']['move'])), 'energy_point', units_stats,team)
                     change_value(instruction[0], ships, peaks, new_coord, 'coordinates', units_stats, team)
                     board[new_coord]['list_entity'].append(instruction[0])
-                    
+                    print()
                     index = board[old_coord]['list_entity'].index(instruction[0])
                     del board[old_coord]['list_entity'][index]
-
-            print(instruction[0] + 'doesn t exist')
             
     return board, ships
             

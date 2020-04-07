46a3ec689ecf0a647dae098179c1da44e3bc1d3a
AnthonyPierard <AnthonyPierard01@users.noreply.github.com>
2020-04-07 15:31:16 +0200

    indentation a la con

--- Play.py
+++ Play.py
@@ -1301,7 +1301,7 @@ def range_verification (units_stats, ship_name, ships, coordinates, team):
 
 def create_order(long, larg,  team, ships, units_stats,peaks) :
 
-     """ This function create the order for the IA
+    """ This function create the order for the IA
 
     Parameter :
     ----------
